69c97a99f29dd297b6d247c13512191a92cae377
sabry <sabry.smk@gmail.com>
2016-02-19 21:21:54 +0300

    Added images for storyboard

---
+++ Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Александр Сабри on 18/02/16.
+//  Copyright © 2016 Александр Сабри. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String{
+        return _name
+    }
+    
+    var pokedexId: Int{
+        return _pokedexId
+    }
+    
+    init (name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
