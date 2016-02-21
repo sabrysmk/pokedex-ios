60af2c631df166e93b72d913f2f5cddabe180cf2
sabry <sabry.smk@gmail.com>
2016-02-21 17:44:18 +0300

    IBOutlets DetailView Controller and Updating Class

--- PokemonDetailVC.swift
+++ PokemonDetailVC.swift
@@ -10,8 +10,17 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
-
-    
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var baseAttack: UILabel!
     var pokemon: Pokemon!
     
     override func viewDidLoad() {
@@ -19,4 +28,8 @@ class PokemonDetailVC: UIViewController {
         
         nameLbl.text = pokemon.name
 }
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
